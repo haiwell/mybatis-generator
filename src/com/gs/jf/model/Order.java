b78fa69b3b51bb75dcbcb2eea0a1b1a1cb6f82bc
wanghai <wangh_2@sina.com>
2018-02-05 23:22:56 +0800

    初始化

---
+++ src/com/gs/jf/model/Order.java
@@ -0,0 +1,106 @@
+package com.gs.jf.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+
+    private String orderNo;
+
+    private String orderName;
+
+    private BigDecimal orderPrice;
+
+    private String description;
+
+    private String remark;
+
+    private String userCode;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String dataStatus;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo == null ? null : orderNo.trim();
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName == null ? null : orderName.trim();
+    }
+
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode == null ? null : userCode.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getDataStatus() {
+        return dataStatus;
+    }
+
+    public void setDataStatus(String dataStatus) {
+        this.dataStatus = dataStatus == null ? null : dataStatus.trim();
+    }
+}
